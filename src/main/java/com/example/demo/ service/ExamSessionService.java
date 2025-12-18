84d72fdf8e0d716b945902dc74d0eed4aa0284d8
bala-amypo <bala@amypo.in>
2025-12-18 05:10:02 +0000

    User commit

--- src/main/java/com/example/demo/ service/ExamSessionService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.ExamSession;
-
-public interface ExamSessionService {
-    ExamSession createSession(ExamSession session);
-    ExamSession getSession(Long sessionId);
-}
