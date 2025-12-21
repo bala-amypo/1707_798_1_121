a46396fc38ead71ed4801992126baf915d6aa8c7
bala-amypo <bala@amypo.in>
2025-12-21 14:24:00 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,41 +1,20 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI examSeatingOpenAPI() {
-        // JWT Bearer security scheme
-        SecurityScheme bearerScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
-        SecurityRequirement securityRequirement = new SecurityRequirement()
-                .addList("bearerAuth");
-
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Exam Seating Arrangement API")
-                        .version("1.0")
-                        .description("API for managing exam seating arrangements"))
+                // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("http://localhost:8080"), // Local server
-                        new Server().url("http://9120.408procr.amypo.ai") // Deployed server
-                ))
-                .addSecurityItem(securityRequirement)
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth", bearerScheme));
-    }
+                        new Server().url("https://9120.408procr.amypo.ai/")
+                ));
+        }
 }
