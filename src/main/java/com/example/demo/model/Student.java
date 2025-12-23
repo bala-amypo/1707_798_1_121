e7abcb83ac4aebe955384faec2a5e5b1e0464b03
bala-amypo <bala@amypo.in>
2025-12-23 09:22:09 +0000

    User commit

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -1,14 +1,9 @@
-package com.example.demo.entity;
+package com.example.demo.model;
 
 import jakarta.persistence.*;
-import lombok.*;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Table(name = "students")
 public class Student {
 
     @Id
@@ -19,4 +14,46 @@ public class Student {
     private String name;
     private String department;
     private int year;
+
+    public Student() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -1,12 +1,7 @@
 package com.example.demo.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
-    Optional<Student> findByRollNumber(String rollNumber);
 }
