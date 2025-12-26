449d70a7d741a1fcff93a2c198a08f272644c883
bala-amypo <bala@amypo.in>
2025-12-26 05:20:49 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -28,6 +28,12 @@ public class SecurityConfig {
 
         return http.build();
     }
+    @Bean
+public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration configuration) throws Exception {
+    return configuration.getAuthenticationManager();
+}
+
 
 
     @Bean
