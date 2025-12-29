37ea8c71e97b326c3dec775336c0f3981e37cd0f
bala-amypo <bala@amypo.in>
2025-12-29 05:19:30 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -27,14 +27,14 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-  
+    
     @Bean
     public AuthenticationManager authenticationManager(
             AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
 
-   
+  
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
